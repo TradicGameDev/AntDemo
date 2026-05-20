46b46d546e027f0d2925be6db05b0803e80a30a3
ragerept <cloudhumura@gmail.com>
2026-05-21 01:04:31 +0300

    sandwich fixing

--- Source/AntDemo/Public/Actors/Sandwich.h
+++ Source/AntDemo/Public/Actors/Sandwich.h
@@ -9,34 +9,34 @@
 class USphereComponent;
 class UStaticMeshComponent;
 
-DECLARE_DYNAMIC_DELEGATE(FOnSandwichCollected);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnSandwichCollected);
 
 UCLASS()
 class ANTDEMO_API ASandwich : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	ASandwich();
-	
-	FOnSandwichCollected OnSandwichCollected;
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    ASandwich();
+    UPROPERTY(BlueprintAssignable)
+    FOnSandwichCollected OnSandwichCollected;
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sandwich")
-	TObjectPtr<UStaticMeshComponent> SandwichMesh;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sandwich")
+    TObjectPtr<UStaticMeshComponent> SandwichMesh;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sandwich")
-	TObjectPtr<USphereComponent> CollisionSphere;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sandwich")
+    TObjectPtr<USphereComponent> CollisionSphere;
 
-	UFUNCTION()
-	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+    UFUNCTION()
+    void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
 };
